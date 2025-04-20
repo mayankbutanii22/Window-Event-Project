814afa033df1bfdccf491d66f5b4191b6ffe63fb
mayankbutanii22 <162984156+mayankbutanii22@users.noreply.github.com>
2025-04-20 09:08:13 +0530

    Add files via upload

---
+++ windowEvent.js
@@ -0,0 +1,10 @@
+function openwindow(){
+    window.open("http:/www.google.com","_blank","width=500px,height=500px,left=200px,top=200px");
+}
+var mywindow;
+function openwindow(){
+    mywindow=window.open("http:/www.google.com","_blank","width=500px,height=500px,left=200px,top=200px");
+}
+function closewindow(){
+    mywindow.close();
+}
